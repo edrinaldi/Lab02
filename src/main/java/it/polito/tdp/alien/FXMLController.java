35655244b0ee61131d0bf045791eb3be33270b45
edoardorinaldi <edoardorinaldi@localhost>
2022-03-16 15:29:46 +0100

    versione iniziale

--- src/main/java/it/polito/tdp/alien/FXMLController.java
+++ src/main/java/it/polito/tdp/alien/FXMLController.java
@@ -6,9 +6,16 @@ package it.polito.tdp.alien;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import it.polito.tdp.alien.model.Model;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class FXMLController {
+	Model model;
 
     @FXML // ResourceBundle that was given to the FXMLLoader
     private ResourceBundle resources;
@@ -16,9 +23,30 @@ public class FXMLController {
     @FXML // URL location of the FXML file that was given to the FXMLLoader
     private URL location;
 
+    @FXML // fx:id="doClearText"
+    private Button doClearText; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtParola"
+    private TextField txtParola; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtTraduzione"
+    private TextArea txtTraduzione; // Value injected by FXMLLoader
+
+    @FXML
+    void doTranslate(ActionEvent event) {
+
+    }
+
     @FXML // This method is called by the FXMLLoader when initialization is complete
     void initialize() {
+        assert doClearText != null : "fx:id=\"doClearText\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtParola != null : "fx:id=\"txtParola\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtTraduzione != null : "fx:id=\"txtTraduzione\" was not injected: check your FXML file 'Scene.fxml'.";
 
+    }
+    
+    public void setModel(Model model) {
+    	this.model = model;
     }
 
 }
